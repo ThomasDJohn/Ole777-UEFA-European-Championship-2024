020efb0c73491aec19de05a36d5ec59e1d1f3942
ThomasDJohn <152852957+ThomasDJohn@users.noreply.github.com>
2023-12-04 15:43:35 +0700

    Add files via upload

---
+++ main/background.js
@@ -0,0 +1,3 @@
+chrome.action.onClicked.addListener(async function() {
+    await chrome.tabs.create({url: 'https://www.google.com/search?hl=en&q=euro+2024+table#sie=lg;/g/11ry54hjmr;2;/m/0zdsnbj;st;fp;1;;;'});
+});
